009e27a64fd64635a3b580d6a42215f5dbaf37f8
senya <senya@SEMYONANUFR7142>
2022-11-26 19:56:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    float a;
+    float b;
+    float c;
+    float volume;
+    float square;
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
+    volume = a * b * c;
+    square = 2 * (a * b + b * c + a * c);
+    std::cout << "Объём параллелепипеда:" << volume << std::endl;
+    std::cout << "Площадь поверхности параллелепипеда:" << square << std::endl;
+}
